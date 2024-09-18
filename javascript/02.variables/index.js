6a6f941985f72aefcbeb238ad6a75eafff6c295c
ferbarcante <barcantef@gmail.com>
2024-09-18 09:38:59 -0300

    tipos

---
+++ javascript/02.variables/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let message = "Hello";
+
+alert(message)
+
+let user = "Maria", age = 25, msg = 'Hello'
---
+++ javascript/03.data-types/index.js
@@ -0,0 +1,22 @@
+"use strict";
+console.log(1/0)
+//resulta infinity
+
+const bigInt = 1234567890123456789012345678901234567890n;
+//o n no final significa Bigint
+
+let name = 'Maria'
+
+//embebed variable
+console.log('Hello, ${name}')
+console.log("the result is ${1 + 2}")
+
+let age
+console.log(age)
+//shows undefined 
+
+console.log(typeof name)
+console.log(typeof 0)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof alert)
