bb5c6fbc16385f06fbbca07539fe84482bb4bc24
ferbarcante <barcantef@gmail.com>
2024-09-18 10:39:41 -0300

    interaction

---
+++ javascript/03.interaction/index.js
@@ -0,0 +1,9 @@
+//alert
+alert('Hello')
+
+//prompt, aceita dois argumentos, o segundo é opcional
+let result = prompt(tittle, 100)
+
+//confirm, question com dois butões
+result = confirm(question)
+
